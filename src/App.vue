a734c24ca1b8b82f19a420f6b07d25f6eeb77fc4
Alberto_Desastre <armassantanaalberto@gmail.com>
2024-11-25 12:28:00 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+  </nav>
+
+  <RouterView />
+</template>
