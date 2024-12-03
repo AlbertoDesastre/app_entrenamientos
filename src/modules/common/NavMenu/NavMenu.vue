de7a683db17a7275cf4f85381909b5775fed58ae
Alberto_Desastre <armassantanaalberto@gmail.com>
2024-12-03 18:15:48 +0000

    added final styles to header menu

--- src/modules/common/NavMenu/NavMenu.vue
+++ src/modules/common/NavMenu/NavMenu.vue
@@ -1,14 +1,15 @@
 <template>
   <section class="header-menu">
     <nav>
-      <article class="burger-menu-mobile">
+      <article class="important-links-container burger-menu-mobile">
         <ul class="dropdown">
+          <li><RouterLink :to="{ name: 'home' }">Home</RouterLink></li>
           <li><RouterLink :to="{ name: 'training' }">Entrenamientos</RouterLink></li>
           <li><RouterLink :to="{ name: 'forum' }">Foro</RouterLink></li>
           <li><RouterLink :to="{ name: 'auth' }">Iniciar sesión</RouterLink></li>
         </ul>
       </article>
-      <RouterLink :to="{ name: 'home' }"><h2>APP!</h2></RouterLink>
+      <!--       <RouterLink :to="{ name: 'home' }"><h2>APP!</h2></RouterLink> -->
       <RouterLink :to="{ name: 'auth' }" id="profile-icon"
         ><img :src="placeholder_person" width="40px" height="40px"
       /></RouterLink>
