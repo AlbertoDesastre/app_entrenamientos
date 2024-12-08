e6ebc192520977768de64aaf8f56803095b9bb31
Alberto_Desastre <armassantanaalberto@gmail.com>
2024-12-08 16:46:07 +0000

    added component ForumComment and ForumView and their CSS

---
+++ src/modules/forum/components/ForumComment/ForumComment.vue
@@ -0,0 +1,31 @@
+<template>
+  <article class="forum-comment">
+    <img :src="props.userAvatar" />
+    <!-- Aunque parezca raro ver un footer al principio, es totalmente correcto!
+     Fuente: https://html.spec.whatwg.org/multipage/sections.html#the-footer-element -->
+    <footer>
+      <p>{{ props.username }}</p>
+      <p>{{ props.creationDate }}, a las {{ props.creationHour }}</p>
+    </footer>
+    <p class="comment">
+      {{ props.comment }}
+    </p>
+  </article>
+</template>
+
+<script setup lang="ts">
+import './ForumComment.css'
+
+interface ForumComment {
+  username: string
+  userAvatar: string
+  comment: string
+  creationDate: string
+  /* En condiciones normales, la fecha vendría en un formato en el que se incluye el año, mes y día MÁS la hora, minutos y segundos
+  Para este proyecto no veo necesario llegar tan lejos en la simulación de datos, así que a mi conveniencia y la de mi tiempo asumo que
+  me llegará el formato que yo quiero: por un lado el año mes y día y por otro horas y minutos.*/
+  creationHour: string
+}
+
+const props = defineProps<ForumComment>()
+</script>
--- src/modules/forum/views/ForumView.vue
+++ src/modules/forum/views/ForumView.vue
@@ -1,3 +1,72 @@
 <template>
-  <h1>This is the Forum View!</h1>
+  <h2>Foro</h2>
+  <h3>Comparte tus comentarios sobre la aplicación en este foro. ¡Todo el mundo tiene acceso!</h3>
+  <section class="forum-wrapper">
+    <ul class="comments-container">
+      <li v-for="comment in forumComments" :key="comment.id">
+        <!--
+        Este "v-bind" es exactamente lo mismo que la opción comentada que hay abajo.
+        Esto es porque los nombres de las prop del componente y los atributos del objeto "comment" son exactamente iguales.
+        -->
+        <ForumComment v-bind="comment" />
+
+        <!--
+        <ForumComment
+          :username="comment.username"
+          :user-avatar="comment.userAvatar"
+          :creation-date="comment.creationDate"
+          :creation-hour="comment.creationHour"
+          :comment="comment.comment"
+        /> -->
+      </li>
+    </ul>
+  </section>
 </template>
+
+<script setup lang="ts">
+import ForumComment from '../components/ForumComment/ForumComment.vue'
+import './ForumView.css'
+
+const forumComments = [
+  {
+    id: 'cmt-001',
+    username: 'Albertito',
+    userAvatar: 'https://via.placeholder.com/80',
+    comment: '¡Este es un comentario muy interesante! Muchas gracias por compartirlo.',
+    creationDate: '2024-12-06',
+    creationHour: '14:35',
+  },
+  {
+    id: 'cmt-002',
+    username: 'MariaGomez',
+    userAvatar: 'https://via.placeholder.com/80/ff7f7f',
+    comment: 'No estoy del todo de acuerdo, pero me encantaría saber más sobre tu opinión.',
+    creationDate: '2024-12-05',
+    creationHour: '18:45',
+  },
+  {
+    id: 'cmt-003',
+    username: 'JuanPerez',
+    userAvatar: 'https://via.placeholder.com/80/87CEEB',
+    comment: 'Gran aporte. Esto resuelve muchas dudas que tenía.',
+    creationDate: '2024-12-06',
+    creationHour: '10:15',
+  },
+  {
+    id: 'cmt-004',
+    username: 'TechGuru',
+    userAvatar: 'https://via.placeholder.com/80/FFD700',
+    comment: 'Revisé los datos que mencionaste y todo parece estar en orden. ¡Buen trabajo!',
+    creationDate: '2024-12-04',
+    creationHour: '22:00',
+  },
+  {
+    id: 'cmt-005',
+    username: 'AnaLopez',
+    userAvatar: 'https://via.placeholder.com/80/90EE90',
+    comment: 'Gracias por la información, me ha servido muchísimo. ¿Tienes alguna fuente adicional?',
+    creationDate: '2024-12-03',
+    creationHour: '16:20',
+  },
+]
+</script>
